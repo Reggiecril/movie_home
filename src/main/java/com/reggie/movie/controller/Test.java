f7c33554c65a9e68cc8e54f072079d1ca1a9631a
reggiecril <reggiecril0618@gmail.com>
2020-09-23 22:08:21 +0800

    add BatisUtil

--- src/main/java/com/reggie/movie/controller/Test.java
+++ src/main/java/com/reggie/movie/controller/Test.java
@@ -1,7 +1,6 @@
 package com.reggie.movie.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.google.common.collect.ImmutableMap;
 import com.reggie.movie.util.JsonUtil;
 
 import java.io.IOException;
@@ -19,23 +18,6 @@ import java.util.stream.Collectors;
  */
 public class Test {
     public static void main(String[] args) {
-        Map<String, String> chineseMap = new ImmutableMap.Builder<String, String>()
-                .put("语言", "language")
-                .put("年份", "publishYear")
-                .put("图片", "image")
-                .put("简介", "brief")
-                .put("评论", "review")
-                .put("添加", "addDate")
-                .put("踩", "unlike")
-                .put("区域", "region")
-                .put("热度排名", "hotRank")
-                .put("主演", "actor")
-                .put("频道", "channel")
-                .put("电影名", "name")
-                .put("导演", "director")
-                .put("赞", "like")
-                .put("分类", "category")
-                .build();
         InputStream is = Test.class.getClassLoader().getResourceAsStream("json/movie_detail.json");
         try {
             List<Map<String, Object>> list = JsonUtil.MAPPER.readValue(is, new TypeReference<List<Map<String, Object>>>() {
